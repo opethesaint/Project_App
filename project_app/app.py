8c490aeebc726d175e2266edca553d834487aa96
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-03-29 07:06:07 +0100

    Refactor app.py to include additional libraries and features

--- project_app/app.py
+++ project_app/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime
 import altair as alt
