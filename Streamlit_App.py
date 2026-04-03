27e424524fbd347d71a38ddb9fd6b81a658a8257
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-03 16:19:55 +0100

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -492,6 +492,29 @@ st.markdown(
 
 
 ##### LAST WORK
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title("My Streamlit App with Live Chat")
+
+# Replace the script below with your own widget code from Tawk.to or Crisp
+chat_code = """
+<!-- Start of Tawk.to Script -->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/YOUR_PROPERTY_ID/DEFAULT';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!-- End of Tawk.to Script -->
+"""
+
+components.html(chat_code, height=600)
 
 
 
